22d7e2e9a4a928c2791bb4c24a8854d97ad3f492
nasu-tatsuya <nasu.tatsuya@dena.jp>
2018-08-31 18:11:05 +0900

    Fix

--- golang/020-map-struct.go
+++ golang/020-map-struct.go
@@ -15,9 +15,18 @@ type Bar struct {
 	Bar int
 }
 
+type Slice struct {
+	Foo   Foo
+	Bar   Bar
+	Slice []int
+}
+
 func main() {
 	a := make(map[Struct]bool)
 	a[Struct{Foo{1}, Bar{1}}] = true
 	a[Struct{Foo{2}, Bar{2}}] = true
 	fmt.Println(a)
+
+	// compile error when a struct includes slice.
+	//b := make(map[Slice]bool)
 }
