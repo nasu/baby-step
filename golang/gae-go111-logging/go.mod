0ca1cd074206905dd17f1eedd7280dfa65cb4a99
nasu-tatsuya <nasu.tatsuya@dena.jp>
2018-10-25 16:39:35 +0900

    Fix

--- golang/gae-go111-logging/go.mod
+++ golang/gae-go111-logging/go.mod
@@ -2,7 +2,7 @@ module github.com/nasu/baby-step/golang/gae-go111-logging
 
 require (
 	cloud.google.com/go v0.30.0 // indirect
-	github.com/googleapis/gax-go v2.0.0+incompatible // indirect
+	contrib.go.opencensus.io/exporter/stackdriver v0.7.0 // indirect
 	go.opencensus.io v0.18.0 // indirect
 	golang.org/x/net v0.0.0-20181017193950-04a2e542c03f // indirect
 	google.golang.org/api v0.0.0-20181021000519-a2651947f503 // indirect
--- golang/gae-go111-logging/main.go
+++ golang/gae-go111-logging/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,25 +10,60 @@ import (
 	"time"
 
 	"cloud.google.com/go/logging"
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+	mrpb "google.golang.org/genproto/googleapis/api/monitoredres"
 )
 
 func main() {
 	prj := os.Getenv("PROJECT")
-	http.HandleFunc("/", index(prj))
+	exporter, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: prj,
+	})
+	if err != nil {
+		panic(err)
+	}
+	view.RegisterExporter(exporter)
+	trace.RegisterExporter(exporter)
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	http.HandleFunc("/", index())
 	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%d", 8080), nil))
 }
 
-func index(prj string) func(http.ResponseWriter, *http.Request) {
+func index() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		client, err := logging.NewClient(context.Background(), prj)
+		hf := &propagation.HTTPFormat{}
+		sc, ok := hf.SpanContextFromRequest(r)
+		if !ok {
+			fmt.Fprintf(w, "error propagation")
+		}
+		ctx, _ := trace.StartSpanWithRemoteParent(r.Context(), "index", sc)
+
+		lc, err := logging.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
 		if err != nil {
+			log.Println(err)
 			return
 		}
-		defer client.Close()
-		lg := client.Logger("mysample")
-		js := []byte(`{"Name": "test", "Count": 3}`)
-		lg.Log(logging.Entry{Payload: json.RawMessage(js), Severity: logging.Info})
-		lg.Log(logging.Entry{Payload: json.RawMessage(js), Severity: logging.Warning})
+		defer lc.Close()
+
+		opts := logging.CommonResource(
+			&mrpb.MonitoredResource{
+				Type: "gae_app",
+				Labels: map[string]string{
+					"project_id": os.Getenv("GOOGLE_CLOUD_PROJECT"),
+					"module_id":  os.Getenv("GAE_SERVICE"),
+					"version_id": os.Getenv("GAE_VERSION"),
+					"zone":       "asia-northeast1-1",
+				},
+			},
+		)
+		lg := lc.Logger("mysample", opts)
+		traceId := [16]byte(sc.TraceID)
+		js := []byte(`{"Name": "test", "Count": 2}`)
+		lg.Log(logging.Entry{Payload: json.RawMessage(js), Severity: logging.Info, Trace: "projects/dena-internal-gdi-gcp/traces/" + hex.EncodeToString(traceId[:])})
+		lg.Log(logging.Entry{Payload: "hogehoge", Severity: logging.Warning, Trace: "projects/dena-internal-gdi-gcp/traces/" + hex.EncodeToString(traceId[:])})
 		lg.StandardLogger(logging.Info).Println("hello")
 		lg.StandardLogger(logging.Warning).Println("hello")
 		log.Println("normal log")
